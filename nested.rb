34045f59a8af39cc0ab73cb1f0b7c38ecabe251e
taroj khan <tarujkhan@yahoo.com>
2020-03-09 14:32:43 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -16,11 +16,15 @@ def hopper
         }
      }
 
+programmer_hash[:grace_hopper]
 
 end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
+	
+
+	 
 	
 	programmer_hash = 
  		{
@@ -37,7 +41,10 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+     	 programmer_hash[:alan_kay][:known_for]
 end
+
 
 def dennis_ritchies_language
 	programmer_hash = 
@@ -55,6 +62,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -80,6 +88,10 @@ def adding_matz
         }
      }
 
+programmer_hash[:yukihiro_matsumoto] = { 
+ :known_for => "Ruby",
+ :languages => ["LISP", "C"]
+}
     
 end
 
@@ -100,10 +112,11 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
+     #alans_new_info = "GUI"
+   end
+     programmer_hash[:alan_kay][:known_for] = "GUI"
      
-     
-end
+
 
 def adding_to_dennis
 	programmer_hash = 
