72b5411430dc9098865b4abe61fb8462eafa03e2
taroj khan <tarujkhan@yahoo.com>
2020-03-09 21:01:49 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -137,7 +137,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:languages] => "Assembly"
+programmer_hash[:languages] << "Assembly"
 programmer_hash
 end
 
